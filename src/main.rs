e4f97d2a61cc596b3ffc2b6bade6e8ac909ec090
oiwn <alex@imscraping.ninja>
2024-06-22 23:54:36 +0700

    trying to figure out app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    RunServer,
+    Test,
+}
+
 fn main() {
-    println!("Hello world");
+    let cli = Cli::parse();
+
+    // Setup log level
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    match &cli.command {
+        Some(Commands::RunServer) => backend::run_server(),
+        Some(Commands::Test) => {
+            println!("Run tests");
+        }
+        None => {}
+    }
 }
