8de738f5026404a47f6f345caef6a6488d467fe5
oiwn <alex@imscraping.ninja>
2024-06-24 18:38:40 +0700

    add cli

--- src/main.rs
+++ src/main.rs
@@ -30,9 +30,50 @@ async fn main() {
         .unwrap();
 }
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    ReadCsv,
+}
+
+fn read_csv(filename: &str) {
+    println!("Reading csv!")
+}
+
 #[cfg(not(feature = "ssr"))]
 pub fn main() {
     // no client-side main function
     // unless we want this to work with e.g., Trunk for a purely client-side app
     // see lib.rs for hydration function instead
+
+    let cli = Cli::parse();
+
+    // TODO: setup logging depending on verbosity level
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::ReadCsv) => {
+            read_csv("data.csv");
+        }
+        None => {}
+    }
 }
